6e6a841f2d86a72a26588f95b796b9ebca832221
Matt Torok <mtorok@berkeley.edu>
2013-08-15 05:48:07 -0700

    Improve ~/.janus/update.sh
    Now, after pulling the latest versions of all the submodules, we run another `git submodule update` to record the new commit pointers into .gitmodules. We also re-build YouCompleteMe.
    
    One future improvement: don't execute the time-consuming YCM build if YCM hasn't actually pulled down new commits.

--- home/.janus/update.sh
+++ home/.janus/update.sh
@@ -3,8 +3,26 @@
 
 pushd `git rev-parse --show-toplevel`
 
+echo "### Syncing"
 git submodule sync
+echo
+
+echo "### Updating"
 git submodule update --recursive --init
+echo
+
+echo "### Pulling"
 git submodule foreach git pull origin master
+echo
+
+echo "### Updating again"
+git submodule update --recursive --init
+echo
+
+echo "## Rebuilding YouCompleteMet"
+cd ./YouCompleteMe
+./install.sh --clang-completer
+cd ..
+
 
 popd
