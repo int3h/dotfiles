113fcd2423bc7e860b4f181c78772b213246c2cc
Matt Torok <mtorok@berkeley.edu>
2013-10-04 17:33:23 -0700

    Vim: fixed update.sh bug which prevented updates
    This bug was preventing the submodules update.sh was supposed to be
    updating from actually checking out the latest commits; they would be
    locked to the current commit.
    
    This was caused because, while update.sh was correctly pulling in the
    latest commits from each submodule, it would then do a `git submodule
    update`, which would then cheeckout the commit of the submodule
    currently recorded in the containing repo.
    
    update.sh was also updated to recompile YCM only if we pulled new
    commits for it. However, it should be noted that if you've pulled newer
    commits than the containing repo has recorded, and did not commit them,
    then update.sh will first checkout the older, recorded commit, then
    pull the newer commits, thus making it think YCM has been updated when
    it may not be any different than the version on disk before update.sh
    was called.

--- home/.janus/update.sh
+++ home/.janus/update.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
+# Takes in a submodule path as an argument, and sets $SUBMOD_REV to the hash of the currently
+# checked out revision of that submodule.
+function getRev {
+    SUBMOD_REV="$(git submodule status $1 | grep -o -P '^[ +-]\K[0-9a-f]+')"
+}
 
-pushd `git rev-parse --show-toplevel`
+function printHeader {
+    printf "\n\e[1m\e[48;5;17m\e[38;5;256m\n### $1\e[0m\n\n"
+}
 
-echo "### Syncing"
+
+pushd `git rev-parse --show-toplevel` > /dev/null
+
+printHeader "Updating submodule URLs to match .gitconfig"
 git submodule sync
-echo
 
-echo "### Updating"
+printHeader "Checking out submodules to recorded commit"
 git submodule update --recursive --init
-echo
 
-echo "### Pulling"
+getRev "home/.janus/YouCompleteMe/"
+OLD_YCM_REV="$SUBMOD_REV"
+
+printHeader "Pulling latest commit of each submodule"
 git submodule foreach git pull origin master
-echo
 
-echo "### Updating again"
-git submodule update --recursive --init
-echo
+getRev "home/.janus/YouCompleteMe"
+NEW_YCM_REV="$SUBMOD_REV"
 
-echo "## Rebuilding YouCompleteMet"
-popd
-pushd ./YouCompleteMe
-./install.sh --clang-completer
-popd
+# Only if we've updated YCM, rebuild it
+if [ $OLD_YCM_REV != $NEW_YCM_REV ]; then
+    printHeader "Rebuilding YouCompleteMe"
+    cd home/.janus/YouCompleteMe
+    ./install.sh --clang-completer
+fi
