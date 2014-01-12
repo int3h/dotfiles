c497ec5ea5c295a8ca7f69e9e1de1b8fb7ea028b
Matt Torok <matt.torok@gmail.com>
2014-01-12 03:35:30 -0800

    Bash: wrap() helper allows extended chars
    In helpers.sh, the wrap() function now allows extended characters in its string arguments. This allows you to, for example, put a newline at the end of the message.

--- home/bin/helpers.sh
+++ home/bin/helpers.sh
@@ -32,7 +32,7 @@ get_script_path() {
 
 # Echoes its arguments to stdout, word-wrapped to the width of the terminal
 wrap() {
-    echo -n "$@" | expand | fold -s -w $(tput cols)
+    echo -ne "$@" | expand | fold -s -w $(tput cols)
 }
 
 
