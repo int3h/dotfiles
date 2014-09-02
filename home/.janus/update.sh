119636a55f088af6d66a47fa2e32309b7b7e69ca
Matt Torok <github@overblown.net>
2014-09-02 15:21:49 -0700

    Vim: .janus/update.sh now accepts 'install' option
    Running `update.sh install` will cause the script to pull and install the recorded version of each submodule. Without this option, the script will also pull the latest version of each submodule from its home repo, then commit that checkout hash to this repo's git config.

--- home/.janus/update.sh
+++ home/.janus/update.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+	printf 'Usage: %s [install]\n\n' "$(basename "$0")"
+	printf 'Install and/or update Vim Janus plugin submodules.\n\n'
+	printf 'Options:\n'
+	printf "  install     don't update submodules to latest version; install as-is\n\n"
+	exit 0
+fi
+
+install_only=
+if [[ $1 == "install" ]]; then
+	install_only=1
+fi
+
 # Takes in a submodule path as an argument, and sets $SUBMOD_REV to the hash of the currently
-# checked out revision of that submodule.
+# checked out revision of that submodule (if any.)
 function getRev {
-    SUBMOD_REV="$(git submodule status $1 | grep -o -P '^[ +-]\K[0-9a-f]+')"
+	# The submodule isn't checked out if its .git file doesn't exist, so return '0' in that case
+	if [[ -f "${1}/.git" ]]; then
+		SUBMOD_REV="$(git submodule status $1 | grep -o -P '^[ +-]\K[0-9a-f]+')"
+	else
+		SUBMOD_REV="0"
+	fi
 }
 
 function printHeader {
@@ -32,20 +50,22 @@ printHeader "Checking out submodules to recorded commit"
 git submodule update --recursive --init
 
 # Save the current YCM revision (before updating) so we can later tell if it was updated
+
 getRev "home/.janus/YouCompleteMe/"
 OLD_YCM_REV="$SUBMOD_REV"
 
-printHeader "Pulling latest commit of each submodule"
-# Updates each submodule to the latest version
-# The 'git pull' could cause a submodule's submodules to need to be updated. Do it with the foreach 
-# so that we only update the 2nd-level submodules, and don't accidentally revert our top-level 
-# submodules back to their recorded commit (undoing the `git pull` here.)
-git submodule foreach 'git pull origin master; git submodule update --recursive --init'
+if [[ $install_only != 1 ]]; then
+	printHeader "Pulling latest commit of each submodule"
+	# Updates each submodule to the latest version
+	# The 'git pull' could cause a submodule's submodules to need to be updated. Do it with the foreach
+	# so that we only update the 2nd-level submodules, and don't accidentally revert our top-level
+	# submodules back to their recorded commit (undoing the `git pull` here.)
+	git submodule foreach 'git pull origin master; git submodule update --recursive --init'
 
 
-printHeader "Committing updates to git repo"
-git add -u
-git commit -m "Vim: update plugins in ~/.janus to latest versions"
+	printHeader "Committing updates to git repo"
+	git add -u
+	git commit -m "Vim: update plugins in ~/.janus to latest versions"
 
 # Re-apply the user's existing changes (stashed at the beginning of this script)
 git stash pop -q
@@ -55,7 +75,7 @@ getRev "home/.janus/YouCompleteMe"
 NEW_YCM_REV="$SUBMOD_REV"
 
 # Only if we've updated YCM, rebuild it
-if [ $OLD_YCM_REV != $NEW_YCM_REV ]; then
+if [[ "$OLD_YCM_REV" != "$NEW_YCM_REV" ]]; then
 	printHeader "Rebuilding YouCompleteMe"
 	cd home/.janus/YouCompleteMe
 	./install.sh --clang-completer
